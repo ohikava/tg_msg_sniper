a773aa1c00605d67b81b95796d710225ef52d27a
Ivan <ivannewest@gmail.com>
2024-03-10 00:23:24 +0300

    added auto buy with bonk

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import os 
+import os
+import time 
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
 from utils import parse_msg
 from observer import Observer
 from sniper import Sniper 
 import logging
-import datetime 
+from datetime import datetime 
+import threading
 
 load_dotenv()
 
@@ -20,15 +22,15 @@ API_HASH = os.getenv("API_HASH")
 CHAT_URL = "@GettingRich7"
 ADMIN_URL = "6587260791" 
 
+bonk = "@bonkbot_bot"
 url = "https://t.me/GettingRich7"
+
 observer = Observer()
 sniper = Sniper()
 client = TelegramClient('anon', API_ID, API_HASH)
 
 @client.on(events.NewMessage(chats=CHAT_URL))
 async def new_message_handler(event):
-    print(f"New message in chat {event.chat_id}: {event.message.text}")
-
     sender_id = event.message.sender_id
             
     if str(sender_id) == ADMIN_URL:
@@ -38,8 +40,19 @@ async def new_message_handler(event):
             observer.send_token_info(contract)
             logger.info(f"Sniping {contract}...")
 
+            await client.send_message(bonk, contract)
 
 
-client.start()
-client.run_until_disconnected()
+def start_bot():
+    client.start()
+    client.run_until_disconnected()
+
+def other_process():
+    while True:
+        if time.time() % 2 == 0:
+            print("Even")
+
+# threading.Thread(target=other_process).start()
+start_bot()
+
 
